52328d1bca51e1a22ef5c99ea77733b264def256
kuro4869 <70685248+kuro4869@users.noreply.github.com>
2021-07-09 20:32:10 +0800

    Update config.py

--- tgfunc/config.py
+++ tgfunc/config.py
@@ -1,6 +1,6 @@
 # RUNTIME
-bot_token = ""
+bot_token = "1816545331:AAG0ITGfcaARRHOIqpFFQNkZ5_JtnqQC82g"
 api_url = 'https://api.telegram.org/bot%s/' % bot_token
 
 # MANAGER CONFIG
-admin_id = 0
+admin_id = 928097164
